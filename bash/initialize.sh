470b5e721388765fbf639dcd9ac7bd66d3032cc4
mastermindzh <Rick@mi-soft.nl>
2015-10-07 16:56:27 +0200

    Added TheRealSimjoo as a collaborator and pushed my latest version

---
+++ bash/initialize.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#clear previous commands
+clear
+
+#print a little welcome page
+echo "*************************************************"
+echo "*                                               *"
+echo "*                                               *"
+echo "*                                               *"
+echo "* Authors: Rick van Lieshout, Ron Nabuurs       *"
+echo "*                                               *"
+echo "*                                               *"
+echo "*                                               *"
+echo "* V2.0                             License: MIT *"
+echo "*************************************************"
+
+#initial welcome message
+echo "Welkom bij het han4pi installatie programma"
