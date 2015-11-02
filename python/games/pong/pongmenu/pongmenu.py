5c045e1dd64121b7783c265448a6255334cfcc88
TheRealSimJoo <simon.mailadres@gmail.com>
2015-11-02 11:55:40 +0100

    Menu aangepast

--- python/games/pong/pongmenu/pongmenu.py
+++ python/games/pong/pongmenu/pongmenu.py
@@ -2,28 +2,36 @@ import pygame
 import mainmenu as menu
 pygame.init()
 
-size = width, height = 340,240	
-screen = pygame.display.set_mode(size)
-screen.fill(0)
-pygame.display.update()
-pygame.key.set_repeat(500,30)
+def bepaalThema(thema):
+  if thema == "Modern":
+	return "Klassiek"
+  return "Modern"
 
-choose = menu.mainmenu(screen, [
+
+size = width, height = 340,240	
+scherm = pygame.display.set_mode(size)
+pygame.key.set_repeat(500,30)
+thema = bepaalThema("")
+
+while True:
+  scherm.fill(0)
+  pygame.display.update()
+  keuze = menu.mainmenu(scherm, [
                         'Twee Speler Spel',
                         'Vier Speler Spel',
-                        'Thema: Klassiek',
+                        'Thema: '+thema,
                         'Highscores',
                         'Spel Beeindigen'], 64,64,None,32,1.4,(0,255,0),(255,0,0))
 
-if choose == 0:
-    print "De gebruiker start een 2 speler spel"
-elif choose == 1:
-    print "De gebruiker start een 4 speler spel"
-elif choose == 2:
-    print "De gebruiker veranderd het thema"
-elif choose == 3:
-    print "De gebruiker vraagt de highscores op"
-elif choose == 4:
-    print "De gebruiker beeindigd het spel"
-pygame.quit()
-exit()
+  if keuze == 0:
+      print "De gebruiker start een 2 speler spel"
+  elif keuze == 1:
+      print "De gebruiker start een 4 speler spel"
+  elif keuze == 2:
+      thema = bepaalThema(thema)
+  elif keuze == 3:
+      print "De gebruiker vraagt de highscores op"
+  elif keuze == 4:
+      print "De gebruiker beeindigd het spel"
+      pygame.quit()
+      exit()
