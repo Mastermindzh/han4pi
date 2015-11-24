57aeaa51b7ec180edb237f51e045fa201338b9b4
Mastermindzh <rick@mi-soft.nl>
2015-11-24 22:35:50 +0100

    I am being stupid. removed reboot

--- install.sh
+++ install.sh
@@ -164,7 +164,7 @@ echo "Zodra u op enter drukt krijgt u tweemaal een wachtwoord invoervak, geef da
 echo "Na het invoeren van het wachtwoord wordt er gevraagd of u een 'view-only' wachtwoord wilt instellen. Geef hier een N in."
 read null
 
-tightvnc :1
+tightvncserver :1
 
 # Voeg tightvnc toe aan de opstartitems
 cp /home/"$USERNAME"/han4pi/bash/resources/tightvnc.desktop /home/"$USERNAME"/.config/autostart/
@@ -176,8 +176,3 @@ cp /home/"$USERNAME"/han4pi/bash/resources/catch_the_raspberry.desktop /usr/shar
 
 echo "cd /home/$USERNAME/\"han4pi/python/games/Catch the raspberry (controller)\"/ && python2 game.py" >> /opt/catch_the_raspberry_controller.sh
 cp /home/"$USERNAME"/han4pi/bash/resources/catch_the_raspberry_controller.desktop /usr/share/applications/Catch_the_raspberry.desktop
-
-
-#herstart
-reboot
-
